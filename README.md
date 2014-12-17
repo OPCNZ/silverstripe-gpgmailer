0c46e5184a4d396506358f396638594665ed0b86
Frank Mullenger <frank@silverstripe.com>
2014-12-17 17:02:25 +1300

    Encryption of plain mail and attachments.

---
+++ README.md
@@ -0,0 +1,87 @@
+# GPG Encryption for Email
+
+## Requirements (see composer.json)
+* Silverstripe Framework 3.1
+* PEAR Crypt GPG 
+
+## Install
+composer require frankmullenger/gpg-mailer dev-master
+
+## Configuration
+Requires keyring files which include 
+- the public key of the recipient of the email which can be used to encrypt the email
+- optionally the private key and public key of the sender which can be used to sign the email
+
+Also optionally need to trust the public key for encrypting. 
+
+Usually the files are:
+- pubring.gpg
+- secring.gpg
+- trustdb.gpg
+
+Options for Crypt_GPG can be added to the YAML file such as the homedir where keyrings are stored, 
+see Crypt_GPGAbstract::__construct() and GPGMailer::setOptions().
+```
+---
+Only:
+  environment: 'dev'
+---
+GPGMailer:
+  options:
+    - homedir: '/Users/someone/Sites/path/'
+      relative_homedir: 'assets/'
+      debug: true
+---
+Only:
+  environment: 'test'
+---
+GPGMailer:
+  options:
+    - homedir: '/Users/someone/Sites/path/'
+      relative_homedir: 'assets/'
+      debug: true
+---
+Only:
+  environment: 'live'
+---
+GPGMailer:
+  options:
+    - homedir: '/Users/someone/Sites/path/'
+      debug: false
+```
+
+## Documentation
+Useful links for uderstanding PGP and creating keys and keyrings:
+- http://www.pgpi.org/doc/pgpintro/
+- https://www.gnupg.org/gph/en/manual/book1.html
+- http://irtfweb.ifa.hawaii.edu/~lockhart/gpg/gpg-cs.html
+
+### Sending encrypted email
+Only plaintext emails can be encrypted, trying to send HTML emails using Email::send() and in turn GPGMailer::sendHTML() 
+triggers a warning and sends the email using GPGMailer::sendPlain().
+
+File attachments are encrypted, we noticed during testing that after encryption the size of the attachment was about 30% 
+larger.
+
+```
+// Create email with plain content
+$content = $this->customise($output)->renderWith('SomeTemplate')->forTemplate();
+$email = new Email(
+	'sender@example.com',
+	'recipient@example.com',
+	$subject,
+	$content
+);
+
+// Set up encryption mailer
+$mailer = new GPGMailer(
+	'frank@swipestripe.com',
+	'sender@example.com', 
+	'senderPa55w0rd'
+);
+$email->set_mailer($mailer);
+
+// HTML emails cannot be encrypted so just send plain
+$result = $email->sendPlain();
+```
+
--- code/GPGMailer.php
+++ code/GPGMailer.php
@@ -2,20 +2,48 @@
 
 require_once 'Crypt/GPG.php';
 
+/**
+ * Mailer that encrypts contents of email using GPG. Encrypting HTML is not implemented, quite difficult and requires 
+ * a very simple HTML template that can be encrypted and re-wrapped in body tags.
+ *
+ * Necessary to provide keyring files via Crypt_GPG options in YAML.
+ */
 class GPGMailer extends Mailer {
 
 	private $options = array();
 
 	private $gpg;
 
-	public function __construct() {
+	private $sign = false;
+
+	/**
+	 * Set options for Crypt_GPG and add encrypting and signing keys.
+	 * 
+	 * @param String $encryptKey        Key identifier, usually an email address but can be fingerprint
+	 * @param String $signKey           Key identifier, usually an email address but can be fingerprint
+	 * @param String $signKeyPassPhrase Optional passphrase for key required for signing
+	 */
+	public function __construct($encryptKey, $signKey = null, $signKeyPassPhrase = null) {
 		parent::__construct();
 
 		// Set options
 		$this->setOptions();
-		$this->gpg = new Crypt_GPG($this->options);		
+		$this->gpg = new Crypt_GPG($this->options);
+
+		// Add encryption key
+		$this->gpg->addEncryptKey($encryptKey);	
+
+		if ($signKey) {
+			$this->gpg->addSignKey($signKey, $signKeyPassPhrase);
+			$this->sign = true;
+		}
 	}
 
+	/**
+	 * Set options for Crypt_GPG.
+	 *
+	 * @see  Crypt_GPGAbstract::__construct()
+	 */
 	private function setOptions() {
 
 		$options = GPGMailer::config()->options;
@@ -30,57 +58,190 @@ class GPGMailer extends Mailer {
 		}
 	}
 
+	/**
+	 * Encrypt and send plain text email, large amount of copy paste from Mailer::sendPlain().
+	 *
+	 * @todo  conversion of BCC -> Bcc necessary in this method as well as sendHTML()?
+	 * 
+	 * @param  String  $to            To address RFC 2822 format
+	 * @param  String  $from          From address RFC 2822 format
+	 * @param  String  $subject       Subject line for email
+	 * @param  String  $plainContent  Content for email
+	 * @param  Boolean $attachedFiles Indicate whether files are attached
+	 * @param  Boolean $customheaders Indicate whether custom headers are presetn
+	 * @return Mixed                  Array if successful or false if unsuccessful
+	 */
 	public function sendPlain($to, $from, $subject, $plainContent, $attachedFiles = false, $customheaders = false) {
 
-		// TODO: Check that the to address is an acceptable encrypt address
-		// TODO: Check that the from address is an acceptable signing address
-		try {
+		// Not ensurely where this is supposed to be set, but defined it false for now to remove php notices
+		$plainEncoding = false; 
 
-			// Encrypt email
-			$encryptKey = $this->getEncryptKey($to);
-			if (!$encryptKey) {
-				throw new Exception("Could not find valid key for encrypting using {$to}.");
+		if ($customheaders && is_array($customheaders) == false) {
+			echo "htmlEmail($to, $from, $subject, ...) could not send mail: improper \$customheaders passed:<BR>";
+			dieprintr($customheaders);
+		}
+	
+		// If the subject line contains extended characters, we must encode it
+		$subject = Convert::xml2raw($subject);
+		$subject = "=?UTF-8?B?" . base64_encode($subject) . "?=";
+
+		// Make the plain text part
+		$headers["Content-Type"] = "text/plain; charset=utf-8";
+		$headers["Content-Transfer-Encoding"] = $plainEncoding ? $plainEncoding : "quoted-printable";
+
+		$plainContent = ($plainEncoding == "base64") 
+			? chunk_split(base64_encode($plainContent), 60)
+			: $this->QuotedPrintable_encode($plainContent);
+
+		// GPG encryption and signing if necessary
+		if ($this->sign) {
+			$plainContent = $this->gpg->encryptAndSign($plainContent);
+		}
+		else {
+			$plainContent = $this->gpg->encrypt($plainContent);
+		}
+
+		// Messages with attachments are handled differently
+		if ($attachedFiles) {
+			// The first part is the message itself
+			$fullMessage = $this->processHeaders($headers, $plainContent);
+			$messageParts = array($fullMessage);
+
+			// Include any specified attachments as additional parts
+			foreach($attachedFiles as $file) {
+				if(isset($file['tmp_name']) && isset($file['name'])) {
+					$messageParts[] = $this->encodeFileForEmail($file['tmp_name'], $file['name']);
+				} else {
+					$messageParts[] = $this->encodeFileForEmail($file);
+				}
 			}
 
-			$this->gpg->addEncryptKey($encryptKey);
-			$encryptedContent = $this->gpg->encrypt($plainContent);
+			// We further wrap all of this into another multipart block
+			list($fullBody, $headers) = $this->encodeMultipart($messageParts, "multipart/mixed");
 
-			// Sign email
-			$signingKey = $this->getSigningKey($from);
-			if (!$signingKey) {
-				throw new Exception("Could not find valid key for signing using {$from}.");
-			}
-
-			$this->gpg->addSignKey($signingKey['email'], $signingKey['password']);
-			$signedContent = $this->gpg->sign($encryptedContent, Crypt_GPG::SIGN_MODE_CLEAR);
-
-			return parent::sendPlain($to, $from, $subject, $signedContent, $attachedFiles, $customheaders);
+		// Messages without attachments do not require such treatment
+		} 
+		else {
+			$fullBody = $plainContent;
 		}
-		catch (Exception $e) {
-			SS_Log::log(new Exception(print_r('Failed to send encrypted email: ' . $e->getMessage(), true)), SS_Log::ERR);
+
+		// Email headers
+		$headers["From"] 		= $this->validEmailAddr($from);
+
+		// Messages with the X-SilverStripeMessageID header can be tracked
+		if(isset($customheaders["X-SilverStripeMessageID"]) && defined('BOUNCE_EMAIL')) {		
+			$bounceAddress = BOUNCE_EMAIL;
+			// Get the human name from the from address, if there is one
+			if(preg_match('/^([^<>]+)<([^<>])> *$/', $from, $parts))
+				$bounceAddress = "$parts[1]<$bounceAddress>";
+		} else {
+			$bounceAddress = $from;
 		}
+		
+		// $headers["Sender"] 		= $from;
+		$headers["X-Mailer"]	= X_MAILER;
+		if(!isset($customheaders["X-Priority"])) {
+			$headers["X-Priority"]	= 3;
+		}
+		
+		$headers = array_merge((array)$headers, (array)$customheaders);
+
+		// the carbon copy header has to be 'Cc', not 'CC' or 'cc' -- ensure this.
+		if (isset($headers['CC'])) { $headers['Cc'] = $headers['CC']; unset($headers['CC']); }
+		if (isset($headers['cc'])) { $headers['Cc'] = $headers['cc']; unset($headers['cc']); }
+
+		// Send the email
+		$headers = $this->processHeaders($headers);
+		$to = $this->validEmailAddr($to);
+
+		// Try it without the -f option if it fails
+		if(!$result = @mail($to, $subject, $fullBody, $headers, "-f$bounceAddress"))
+			$result = mail($to, $subject, $fullBody, $headers);
+		
+		if($result)
+			return array($to,$subject,$fullBody,$headers);
+			
+		return false;
 	}
 
-	private function getEncryptKey($address) {
+	/**
+	 * Encrypting HTML emails does not work so this method triggers a warning and sends using sendPlain() and plaintext 
+	 * version of the HTML content.
+	 */
+	public function sendHTML($to, $from, $subject, $htmlContent, $attachedFiles = false, $customheaders = false, $plainContent = false) {
 
-		$key = null;
-		if (in_array($address, $this->config()->encrypt_keys)) {
-			$key = $address;
+		// HTML emails cannot be encrypted and create a number of issues, sendPlain() should be used instead
+		trigger_error('HTML email content cannot be encrypted, only the plain text component of this email will be generated.', E_USER_WARNING);
+
+		if (!$plainContent) {
+			$plainContent = Convert::xml2raw($htmlContent);
 		}
-		return $key;
+
+		return $this->sendPlain($to, $from, $subject, $plainContent, $attachedFiles, $customheaders);
 	}
 
-	private function getSigningKey($address) {
+	/**
+	 * Encode file for email, encryption results in ASCII armored data which removed need for base 64 encoding step.
+	 *
+	 * @todo  test with filename instead of array passed as $file, see Email::attachFile() and ::attachFileFromString()
+	 * @todo  test with $destFilename
+	 * @todo  test with disposition set to inline
+	 * @todo  test with contentLocation param, see Mailer::encodeFileForEmail()
+	 * @todo  test with different types of files which use different MIME types
+	 * @todo  test with large files
+	 * @todo  test with script timeout
+	 * 
+	 * @param  Mixed   $file         Array of file data including content or just string indicating filename
+	 * @param  String  $destFileName Destination filename
+	 * @param  String  $disposition  Disposition of attachment, inline or attachment
+	 * @param  String  $extraHeaders Extra headers for attachement
+	 * @return String                Contents for attachement including headers and ASCII armored file content
+	 */
+	public function encodeFileForEmail($file, $destFileName = false, $disposition = NULL, $extraHeaders = "") {
 
-		$key = array();
-
-		foreach ($this->config()->signing_keys as $values) {
-			if ($values['email'] == $address) {
-				$key = $values;
-				break;
+		if (!$file) {
+			user_error("encodeFileForEmail: not passed a filename and/or data", E_USER_WARNING);
+			return;
+		}
+		
+		if (is_string($file)) {
+			$file = array('filename' => $file);
+			$fh = fopen($file['filename'], "rb");
+			if ($fh) {
+				$file['contents'] = "";
+				while(!feof($fh)) $file['contents'] .= fread($fh, 10000);	
+				fclose($fh);
 			}
 		}
-		return $key;
+
+		// Build headers, including content type
+		if (!$destFileName) $base = basename($file['filename']);
+		else $base = $destFileName;
+
+		// Force base and MIME type for encrypted attachements
+		$base = $base . '.pgp';
+		$mimeType = 'application/octet-stream';
+
+		// TODO Need to test with contentLocation param
+		if (empty($disposition)) $disposition = isset($file['contentLocation']) ? 'inline' : 'attachment';
+
+		// Encryption results in ASCII armored data appropriate for email
+		if ($this->sign) {
+			$file['contents'] = $this->gpg->encryptAndSign($file['contents']);
+		}
+		else {
+			$file['contents'] = $this->gpg->encrypt($file['contents']);
+		}
+
+		$headers =	"Content-type: $mimeType;\n\tname=\"$base\"\n".
+			"Content-Transfer-Encoding: $encoding\n".
+			"Content-Disposition: $disposition;\n\tfilename=\"$base\"\n";
+		
+		// TODO Need to test with contentLocation param
+		if ( isset($file['contentLocation']) ) $headers .= 'Content-Location: ' . $file['contentLocation'] . "\n" ;
+		
+		$headers .= $extraHeaders . "\n";
+		return $headers . $file['contents'];
 	}
 
 }
