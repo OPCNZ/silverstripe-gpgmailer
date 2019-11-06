e6e00b017123267cefb200c01222e54c3583c2da
Dylan Wagstaff <dylan@silverstripe.com>
2019-11-06 17:13:38 +1300

    MAJOR upgrade to Silverstripe 4 - via SwiftMailer
    Silverstripe CMS 4 has updated its mailing backend, which is now powered by SwiftMailer.
    SwiftMailer has existing support for signers and encryption, and via this interface the GPGMailer
    module has been cleaned up a lot. This also now implements a PGP/MIME message format, as defined
    by RFC 3156.
    
    Reference:
     - https://tools.ietf.org/html/rfc3156
     - https://www.mailpile.is/blog/2014-11-21_To_PGP_MIME_Or_Not.html

--- docs/en/index.md
+++ docs/en/index.md
@@ -26,25 +26,36 @@ config options see `Crypt_GPGAbstract::__construct()`.
 
 #### Environment settings
 
-These should be defined in your `_ss_environment.php` file, above your webroot. Don't store passphrases in Git!
+These should be defined in your envrionment, or `.env` file above your project's root; Don't store passphrases in Git!
 
 | Constant | Example | Explanation |
 |----------|---------|-------------|
 | `GPGMAILER_HOMEDIR` | `'/path/to/keyring/'` | Absolute path of the folder containing GPG keyring(s) |
 | `GPGMAILER_ENCRYPT_KEY` | `'recipient@example.com'` | ID (usually email address) of the recipient's key |
 | `GPGMAILER_SIGN_KEY` | `'sender@example.com'` | ID (usually email address) of the sender's key _(optional)_ |
-| `GPGMAILER_SIGN_KEY_PASSPHRASE` | `'senderPa55w0rd'` | Passphrase for the sender's private key _(optional)_ |
+| `GPGMAILER_SIGN_KEY_PASSPHRASE` | `'Y!M7oNG^w3x6dxUpu0u^'` | Passphrase for the sender's private key _(optional)_ |
 
 ### Sending encrypted email
 
 Once you have generated your GPG keys and correctly configured SilverStripe CMS you can use `GPGMailer` class to encrypt
 and sign emails.
 
-Only plaintext emails can be encrypted, trying to send HTML emails using `Email::send()` and in turn
-`GPGMailer::sendHTML()` triggers a warning and sends the email using `GPGMailer::sendPlain()`.
-
 File attachments are also encrypted. During testing the size of the encrypted attachment was about 30% larger than the
 original.
+
+#### Available options of Crypt_GPG 1.6.3 are:
+
+- `string  homedir` - the directory where the GPG keyring files are stored. If not specified, Crypt_GPG uses the default of `~/.gnupg`.
+- `string  publicKeyring`  - the file path of the public keyring. Use this if the public keyring is not in the homedir, or if the keyring is in a directory not writable by the process invoking GPG (like Apache). Then you can specify the path to the keyring with this option (/foo/bar/pubring.gpg), and specify a writable directory (like /tmp) using the <i>homedir</i> option.
+- `string  privateKeyring` - the file path of the private keyring. Use this if the private keyring is not in the homedir, or if the keyring is in a directory not writable by the process invoking GPG (like Apache). Then you can specify the path to the keyring with this option (/foo/bar/secring.gpg), and specify a writable directory (like /tmp) using the <i>homedir</i> option.
+- `string  trustDb` - the file path of the web-of-trust database. Use this if the trust database is not in the homedir, or if the database is in a directory not writable by the process invoking GPG (like Apache). Then you can specify the path to the trust database with this option (/foo/bar/trustdb.gpg), and specify a writable directory (like /tmp) using the <i>homedir</i> option.
+- `string  binary`  - the location of the GPG binary. If not specified, the driver attempts to auto-detect the GPG binary location using a list of known default locations for the current operating system. The option `gpgBinary` is a deprecated alias for this option.
+- `string  agent`   - the location of the GnuPG agent binary. The gpg-agent is only used for GnuPG 2.x. If not specified, the engine attempts to auto-detect the gpg-agent binary location using a list of know default locations for the current operating system.
+- `string|false gpgconf`   - the location of the GnuPG conf binary. The gpgconf is only used for GnuPG >= 2.1. If not specified, the engine attempts to auto-detect the location using a list of know default locations. When set to FALSE `gpgconf --kill` will not be executed via destructor.
+- `string digest-algo`     - Sets the message digest algorithm.
+- `string cipher-algo`     - Sets the symmetric cipher.
+- `boolean strict`  - In strict mode clock problems on subkeys and signatures are not ignored (--ignore-time-conflict and --ignore-valid-from options)
+- `mixed debug`     - whether or not to use debug mode. When debug mode is on, all communication to and from the GPG subprocess is logged. This can be useful to diagnose errors when using Crypt_GPG.
 
 ### Implementation example
 
@@ -64,5 +75,3 @@ $email->set_mailer(new GPGMailer());
 // HTML emails cannot be encrypted so just send plain
 $result = $email->sendPlain();
 ```
-
-
--- src/GPGMailer.php
+++ src/GPGMailer.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace Silverstripe\GPGMailer;
+namespace OPCNZ\GPGMailer;
 
-use Crypt_GPG;
-use InvalidArgumentException;
 use Exception;
+use InvalidArgumentException;
+use Nightjar\SwiftSignerCryptGPG;
 use SilverStripe\Control\Director;
+use SilverStripe\Control\Email\SwiftMailer;
 use SilverStripe\Core\Convert;
-use SilverStripe\Control\Email\Mailer;
-
-require_once 'Crypt/GPG.php';
+use SilverStripe\Core\Environment;
+use Swift_Mailer;
 
 /**
  * Mailer that encrypts contents of email using GPG. Encrypting HTML is not implemented, quite difficult and requires
@@ -29,316 +29,153 @@ require_once 'Crypt/GPG.php';
  * @todo  Content-Type header to include protocol="application/pgp-encrypted" https://tools.ietf.org/html/rfc3156
  *
  */
-class GPGMailer implements Mailer
+class GPGMailer extends SwiftMailer
 {
+    private static $dependencies = [
+        'SwiftMailer' => '%$' . Swift_Mailer::class,
+    ];
 
     /**
      * Options for Crypt_GPG
      *
      * @see Crypt_GPGAbstract::__construct() for available options
+     *
+     * @config
      * @var array
      */
-    private $options = array();
+    private static $default_options = [];
 
     /**
-     * Instance of Crypt_GPG
+     * Options provided to Crypt_GPG
      *
-     * @var Crypt_GPG
+     * @var array
      */
-    private $gpg;
+    private $cryptGPGOptions = [];
 
     /**
-     * Whether to sign the email also
+     * Encryption key for the email
      *
      * @var boolean
      */
-    private $sign = false;
+    private $encryptKey;
+
+    /**
+     * Whether to sign the email with this key also
+     *
+     * @var boolean
+     */
+    private $signKey;
+
+    /**
+     * Passphrase for signing key, if needed
+     *
+     * @var boolean
+     */
+    private $signKeyPassphrase;
 
     /**
      * Set options for Crypt_GPG and add encrypting and signing keys.
      *
      * @param string $encryptKey        Key identifier, usually an email address but can be fingerprint
      * @param string $signKey           Key identifier, usually an email address but can be fingerprint
-     * @param string $signKeyPassphrase Optional passphrase for key required for signing
+     * @param string $signKeyPassphrase Passphrase required for the signKey
+     * @param array  $options           Option set {@see Crypt_GPGAbstract::__construct}
+     * @throws InvalidArgumentException
      */
-    public function __construct($encryptKey = null, $signKey = null, $signKeyPassphrase = null)
+    public function __construct($encryptKey = '', $signKey = '', $signKeyPassphrase = '', $options = [])
     {
-        parent::__construct();
+        $this->setCryptGPGOptions($options);
+        $this->setEncryptKey($encryptKey ?: Environment::getEnv('GPGMAILER_ENCRYPT_KEY'));
+        $this->setSignKey($signKey ?: Environment::getEnv('GPGMAILER_SIGN_KEY'));
+        $this->setSignKeyPassphrase($signKeyPassphrase ?: Environment::getEnv('GPGMAILER_SIGN_KEY_PASSPHRASE'));
+    }
 
-        // Set options
-        $this->setOptions();
-        $this->gpg = new Crypt_GPG($this->options);
-
-        // Add encryption key
-        if (is_null($encryptKey) && !defined('GPGMAILER_ENCRYPT_KEY')) {
-            throw new InvalidArgumentException('$encryptKey not defined');
+    /**
+     * Sets the encryption key for this mailer.
+     *
+     * @param string|null $encryptionKey
+     * @return $this
+     */
+    public function setEncryptKey($encryptionKey = null)
+    {
+        if (!isset($encryptionKey)) {
+            throw new InvalidArgumentException('Encryption key not defined');
         }
-        $this->gpg->addEncryptKey($encryptKey ?: GPGMAILER_ENCRYPT_KEY);
+        $this->encryptKey = $encryptionKey;
+        return $this;
+    }
 
-        // Add signing key
-        if ($signKey || defined('GPGMAILER_SIGN_KEY')) {
-            if (is_null($signKeyPassphrase) && defined('GPGMAILER_SIGN_KEY_PASSPHRASE')) {
-                $signKeyPassphrase = GPGMAILER_SIGN_KEY_PASSPHRASE;
-            }
-            $this->gpg->addSignKey($signKey ?: GPGMAILER_SIGN_KEY, $signKeyPassphrase);
-            $this->sign = true;
-        }
+    /**
+     * Set signing key for this mailer. Optional.
+     *
+     * @param string|null $signingKey
+     * @return $this
+     */
+    public function setSignKey($signingKey = null)
+    {
+        // set explicitly to null as empty string may be used as an invalid key
+        $this->signKey = $signingKey === '' ? null : $signingKey;
+        return $this;
+    }
+
+    /**
+     * Set sign key passphrase if applicable
+     *
+     * @param string|null $passphrase
+     * @return $this
+     */
+    public function setSignKeyPassphrase($passphrase = null)
+    {
+        // set explicitly to null as empty string may be used as an invalid passphrase
+        $this->signKeyPassphrase = $passphrase === '' ? null : $passphrase;
+        return $this;
     }
 
     /**
      * Set options for Crypt_GPG.
      *
-     * @see Crypt_GPGAbstract::__construct() for available options
+     * Some options are always overridden if environment variables are present. This allows for ease of set up in
+     * testing envrionments, providing assurance of settings.
+     *
+     * @param array $options Option set. {@see Crypt_GPGAbstract::__construct} for available options
+     * @return $this
      */
-    private function setOptions()
+    public function setCryptGPGOptions(array $options = []): self
     {
-        $options = GPGMailer::config()->options;
-        if (isset($options[0]) && is_array($options[0])) {
-            $this->options = $options[0];
+        $options = $options ?: $this->config()->get('default_options');
+
+        if ($homeDir = Environment::getEnv('GPGMAILER_HOMEDIR')) {
+            // Environment variables should override Configuration system
+            $options['homedir'] = $homeDir;
+        } else if (isset($options['relative_homedir'])) {
+            // Option to override home dir and provide a relative path instead
+            $options['homedir'] = Director::getAbsFile($options['relative_homedir']);
+            unset($options['relative_homedir']);
         }
 
-        // Option to override home dir and provide a relative path instead
-        if (isset($this->options['relative_homedir'])) {
-            $this->options['homedir'] = Director::getAbsFile($this->options['relative_homedir']);
-            unset($this->options['relative_homedir']);
-        }
+        $this->cryptGPGOptions = $options;
 
-        // Environment variables should override Configuration system
-        if (defined('GPGMAILER_HOMEDIR')) {
-            $this->options['homedir'] = GPGMAILER_HOMEDIR;
-        }
+        return $this;
     }
 
     /**
-     * Encrypt and send plain text email, large amount of copy paste from Mailer::sendPlain().
-     *
-     * @todo  conversion of BCC -> Bcc necessary in this method as well as sendHTML()?
-     *
-     * @param  string  $to            To address RFC 2822 format
-     * @param  string  $from          From address RFC 2822 format
-     * @param  string  $subject       Subject line for email
-     * @param  string  $plainContent  Content for email
-     * @param  boolean $attachedFiles Indicate whether files are attached
-     * @param  array   $customheaders Custom email headers
-     * @return mixed                  Array if successful or false if unsuccessful
+     * @param Email $message
+     * @return bool Whether the sending was "successful" or not
      */
-    public function sendPlain($to, $from, $subject, $plainContent, $attachedFiles = false, $customheaders = false)
+    public function send($message)
     {
+        /** @var Swift_Message $swiftMessage */
+        $swiftMessage = $message->getSwiftMessage();
+        $swiftMessage->attachSigner(new SwiftSignerCryptGPG(
+            $this->encryptKey,
+            $this->signKey,
+            $this->signKeyPassphrase,
+            $this->cryptGPGOptions
+        ));
+        $failedRecipients = array();
+        $result = $this->sendSwift($swiftMessage, $failedRecipients);
+        $message->setFailedRecipients($failedRecipients);
 
-        // Not ensurely where this is supposed to be set, but defined it false for now to remove php notices
-        $plainEncoding = false;
-
-        if ($customheaders && is_array($customheaders) == false) {
-            user_error("Could not send mail, improper custom headers: $customheaders", E_USER_WARNING);
-            return false;
-        }
-
-        // If the subject line contains extended characters, we must encode it
-        $subject = Convert::xml2raw($subject);
-        $subject = "=?UTF-8?B?" . base64_encode($subject) . "?=";
-
-        // Make the plain text part
-        $headers["Content-Type"] = "text/plain; charset=utf-8";
-
-        // Encoding forced to 7bit
-        $headers["Content-Transfer-Encoding"] = "7bit";
-
-        // GPG encryption and signing if necessary
-        if ($this->sign) {
-            $plainContent = $this->gpg->encryptAndSign($plainContent);
-        } else {
-            $plainContent = $this->gpg->encrypt($plainContent);
-        }
-
-        // Messages with attachments are handled differently
-        if ($attachedFiles) {
-            // The first part is the message itself
-            $fullMessage = $this->processHeaders($headers, $plainContent);
-            $messageParts = array($fullMessage);
-
-            // Include any specified attachments as additional parts
-            foreach ($attachedFiles as $file) {
-                if (isset($file['tmp_name']) && isset($file['name'])) {
-                    $messageParts[] = $this->encodeFileForEmail($file['tmp_name'], $file['name']);
-                } else {
-                    $messageParts[] = $this->encodeFileForEmail($file);
-                }
-            }
-
-            // We further wrap all of this into another multipart block
-            list($fullBody, $headers) = $this->encodeMultipart($messageParts, "multipart/mixed");
-
-        // Messages without attachments do not require such treatment
-        } else {
-            $fullBody = $plainContent;
-        }
-
-        // Email headers
-        $headers["From"]        = $this->validEmailAddr($from);
-
-        // Messages with the X-SilverStripeMessageID header can be tracked
-        if (isset($customheaders["X-SilverStripeMessageID"]) && defined('BOUNCE_EMAIL')) {
-            $bounceAddress = BOUNCE_EMAIL;
-            // Get the human name from the from address, if there is one
-            if (preg_match('/^([^<>]+)<([^<>])> *$/', $from, $parts)) {
-                $bounceAddress = "$parts[1]<$bounceAddress>";
-            }
-        } else {
-            $bounceAddress = $from;
-        }
-
-        // $headers["Sender"] 		= $from;
-        $headers["X-Mailer"]    = X_MAILER;
-        if (!isset($customheaders["X-Priority"])) {
-            $headers["X-Priority"]    = 3;
-        }
-
-        $headers = array_merge((array)$headers, (array)$customheaders);
-
-        // the carbon copy header has to be 'Cc', not 'CC' or 'cc' -- ensure this.
-        if (isset($headers['CC'])) {
-            $headers['Cc'] = $headers['CC'];
-            unset($headers['CC']);
-        }
-        if (isset($headers['cc'])) {
-            $headers['Cc'] = $headers['cc'];
-            unset($headers['cc']);
-        }
-
-        // Send the email
-        $headers = $this->processHeaders($headers);
-        $to = $this->validEmailAddr($to);
-
-        // Try it without the -f option if it fails
-        if (!$result = @mail($to, $subject, $fullBody, $headers, "-f$bounceAddress")) {
-            $result = mail($to, $subject, $fullBody, $headers);
-        }
-
-        if ($result) {
-            return array($to,$subject,$fullBody,$headers);
-        }
-
-        return false;
-    }
-
-    /**
-     * Encrypting HTML emails does not work so this method triggers a warning and sends using sendPlain() and plaintext
-     * version of the HTML content.
-     *
-     * @param  string  $to            To address RFC 2822 format
-     * @param  string  $from          From address RFC 2822 format
-     * @param  string  $subject       Subject line for email
-     * @param  string  $plainContent  Content for email
-     * @param  boolean $attachedFiles Indicate whether files are attached
-     * @param  array   $customheaders Custom email headers
-     * @return mixed                  Array if successful or false if unsuccessful
-     */
-    public function sendHTML($to, $from, $subject, $htmlContent, $attachedFiles = false, $customheaders = false, $plainContent = false)
-    {
-
-        // HTML emails cannot be encrypted and create a number of issues, sendPlain() should be used instead
-        trigger_error('HTML email content cannot be encrypted, only the plain text component of this email will be generated.', E_USER_WARNING);
-
-        if (!$plainContent) {
-            $plainContent = Convert::xml2raw($htmlContent);
-        }
-
-        return $this->sendPlain($to, $from, $subject, $plainContent, $attachedFiles, $customheaders);
-    }
-
-    /**
-     * Encode file for email, encryption results in ASCII armored data which removed need for base 64 encoding step.
-     *
-     * @todo  test with filename instead of array passed as $file, see Email::attachFile() and ::attachFileFromString()
-     * @todo  test with $destFilename
-     * @todo  test with disposition set to inline
-     * @todo  test with contentLocation param, see Mailer::encodeFileForEmail()
-     *
-     * @param  mixed   $file         Array of file data including content or just string indicating filename
-     * @param  string  $destFileName Destination filename
-     * @param  string  $disposition  Disposition of attachment, inline or attachment
-     * @param  string  $extraHeaders Extra headers for attachement
-     * @return string                Contents for attachement including headers and ASCII armored file content
-     */
-    public function encodeFileForEmail($file, $destFileName = false, $disposition = null, $extraHeaders = "")
-    {
-        if (!$file) {
-            user_error("encodeFileForEmail: not passed a filename and/or data", E_USER_WARNING);
-            return;
-        }
-
-        if (is_string($file)) {
-            $file = array('filename' => $file);
-            $fh = fopen($file['filename'], "rb");
-            if ($fh) {
-                $file['contents'] = "";
-                while (!feof($fh)) {
-                    $file['contents'] .= fread($fh, 10000);
-                }
-                fclose($fh);
-            }
-        }
-
-        // Build headers, including content type
-        if (!$destFileName) {
-            $base = basename($file['filename']);
-        } else {
-            $base = $destFileName;
-        }
-
-        // Force base and MIME type for encrypted attachements
-        $base = $base . '.pgp';
-        $mimeType = 'application/octet-stream';
-
-        // TODO Need to test with contentLocation param
-        if (empty($disposition)) {
-            $disposition = isset($file['contentLocation']) ? 'inline' : 'attachment';
-        }
-
-        // Encode for emailing. Only accepts binary|8bit|7bit not quoted-printable|base64
-        // ASCII armored output *should* be base64 though?
-        $encoding = "7bit";
-
-        // GPG encryption and signing if necessary
-        if ($this->sign) {
-            $file['contents'] = $this->gpg->encryptAndSign($file['contents']);
-        } else {
-            $file['contents'] = $this->gpg->encrypt($file['contents']);
-        }
-
-        $headers =    "Content-type: $mimeType;\n\tname=\"$base\"\n".
-            "Content-Transfer-Encoding: $encoding\n".
-            "Content-Disposition: $disposition;\n\tfilename=\"$base\"\n";
-
-        // TODO Need to test with contentLocation param
-        if (isset($file['contentLocation'])) {
-            $headers .= 'Content-Location: ' . $file['contentLocation'] . "\n" ;
-        }
-
-        $headers .= $extraHeaders . "\n";
-        return $headers . $file['contents'];
-    }
-
-    /**
-     * Handle the renaming of the validEmailAddr method in silverstripe-framework 3.2.0.
-     * Maintains backwards compatibility with silverstripe-framework 3.1.
-     *
-     * @see Mailer::validEmailAddress
-     */
-    public function validEmailAddr($address)
-    {
-        if (method_exists(Mailer::class, 'validEmailAddr')) {
-            return parent::validEmailAddr($address);
-        } elseif (method_exists(Mailer::class, 'validEmailAddress')) {
-            return parent::validEmailAddress($address);
-        } else {
-            throw new Exception('validEmailAddr (or validEmailAddress) method not found on Mailer');
-        }
-    }
-
-    public function send($email)
-    {
-        /** @upgrade */
-        throw new \RuntimeException('Not implement/upgraded yet');
+        return $result != 0;
     }
 }
